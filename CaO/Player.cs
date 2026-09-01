56e4be23671ace08d3d3df39d8f28e966b78307c
desjardinsm <desjardinsm@etsu.edu>
2026-09-01 16:03:01 -0400

    First half of class!

---
+++ CaO/Player.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaO
+{
+    public class Player
+    {
+
+        //private string name;
+
+        public string Name { get; init; }
+
+        private int _health;
+        public int Health
+        {
+            get
+            {
+                return _health;
+            }
+            set
+            {
+                if (value < 0)
+                    value = 1;
+
+                this._health = value;
+            }
+        }
+
+
+
+        //+ Player()
+
+
+        public Player()
+        {
+            Name = "Default";
+            Health = 100;
+        }
+
+
+        //+ Player(name:string)
+        public Player(string name)
+        {
+            Name = name;
+            Health = 100;
+        }
+
+        //+ Player(name:string, health:int)
+        public Player(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+
+
+
+        /*   public int GetHealh()
+           {
+               return health;
+           }
+
+           public void SetHealth(int health)
+           {
+               if(health < 0)
+                   health = 1;
+
+               this.health = health;
+           }*/
+
+
+    }
+}
--- CaO/Program.cs
+++ CaO/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using CaO;
+using System.Numerics;
+
+
+Player p1 = new Player("Link");
+Player p2 = new Player("Link");
+
+Player p3 = p2;
+
+//Console.WriteLine(p1.GetHealh());
+
+p1.Health = 100;
+
+Console.WriteLine($"Player 1's name is {p1.Name}");
+Console.WriteLine($"Player 1's health is {p1.Health}");
+
+
+//p1.Name = "Zelda"; // This line will cause a compilation error because Name has an init-only setter."
